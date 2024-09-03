7ae67e2e14fd2284ded519f2885fdd1343def4b7
uaerd <133880865+uaerd@users.noreply.github.com>
2024-09-03 18:38:14 +0700

    Basic Update

---
+++ dragon-dungeon-1.20.1/src/main/java/com/dragon/dungeon/DragonDungeonDataGenerator.java
@@ -0,0 +1,11 @@
+package com.dragon.dungeon;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DragonDungeonDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
--- dragon-dungeon-1.20/src/client/kotlin/com/dragon/dungeon/DragonDungeonClient.kt
+++
@@ -1,9 +0,0 @@
-package com.dragon.dungeon
-
-import net.fabricmc.api.ClientModInitializer
-
-object DragonDungeonClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
