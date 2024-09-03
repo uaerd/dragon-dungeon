d1f65a6ea4d983dd05693a2676bcf4a909fb3e90
uaerd <133880865+uaerd@users.noreply.github.com>
2024-09-03 18:10:02 +0700

    Basic update

---
+++ dragon-dungeon-1.20/src/client/kotlin/com/dragon/dungeon/DragonDungeonClient.kt
@@ -0,0 +1,9 @@
+package com.dragon.dungeon
+
+import net.fabricmc.api.ClientModInitializer
+
+object DragonDungeonClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
